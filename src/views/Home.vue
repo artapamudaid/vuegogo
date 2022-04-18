762096598ca808e68ab18302384207378a17f13d
artapamudaid <pamuda.id@gmail.com>
2022-04-18 11:42:55 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <h3>My Activities</h3>
+    <div class="pekerjaan-rumah">
+      <div v-for="tugasSaya in tugas" v-bind:key="tugasSaya.id" class="tugas">
+        <p>{{ tugasSaya.nama }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  data: function () {
+    return {
+      tugas: [
+      {
+        id: 1,
+        nama: 'Menyapu Rumah'
+      },
+      {
+        id: 2,
+        nama: 'Menguras Bak Mandi'
+      },
+      {
+        id: 3,
+        nama: 'Mencuci Piring'
+      }
+    ]
+    }
+  },
+  components: {
+  }
+}
+</script>
---
+++ src/views/Profile.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="profile">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <div class="d-flex align-items-center">
+                    <div class="flex-shrink-0">
+                        <img src="../assets/img/my-pic.jpeg" alt="image" class="user-pic mr-3">
+                    </div>
+                    <div class="flex-grow-1 ms-3">
+                        <h5>Arta Tri Pamuda</h5>
+                        <p>This is some content from a media component. You can replace this with any content and adjust it as needed.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+export default {
+  name: 'Profile',
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+.user-pic {
+    height: 80px;
+    widows: 80px;
+    border-radius: 200%;
+}
+</style>
